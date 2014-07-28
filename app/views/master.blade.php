99dff47522a1239abfcec5c1f03c34c440583a72
Jesus Tadena <jestadena@yahoo.com>
2014-07-28 09:34:41 +0800

    fixed sidebar not scrolling. added responsive setting

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -6,8 +6,7 @@
 	@include('global/css')
 	<style>
 		body {
-			background-color: #222222;
-			margin-top:30px;
+			background-color: #222222; margin-top:30px;
 		}
 		.container-fluid {
 			width: 1366px;
@@ -19,33 +18,52 @@
 			padding: 10px;
 		}
 		.sidebar {
+			position: relative;
 			background-color: #B1B1B1;
-			padding: 10px;
 		}
 		.logo {
 			margin-left: 20px;
+		}
+
+		/* Large desktop */
+		@media (min-width: 1200px) {
+	
+		}
+
+		/* Portrait tablet to landscape and desktop */
+		@media (min-width: 768px) and (max-width: 979px) {
+		}
+
+		/* Landscape phone to portrait tablet */
+		@media (max-width: 767px) {
+			.container-fluid {
+				width: auto;
+			}
+		}
+
+		/* Landscape phones and down */
+		@media (max-width: 480px) {
+			.container-fluid {
+				width: auto;
+			}
 		}
 	</style>
 </head>
 <body>
 	<div class="container-fluid">
-		<div class="row">
+		<div class="row wrapper">
 			<!-- START: header -->
 			<div class="header">
 			@include('page/header')
 			</div>
 			<!-- END: header -->
 			<!-- START: Sidebar -->
-			<div class="col-md-2 sidebar">
-				<div class="logo">
-				@include('page/logo')
-				</div>
-			@include('page/sidebar')
-			@include('page/footer')
+			<div class="sidebar col-md-2 col-sm-2">
+			 	@include('page/sidebar')
 			</div>
 			<!-- End: Sidebar -->
 			<!-- START: Content -->
-			<div class="col-md-10 content">
+			<div class="content col-md-10 col-sm-10">
 			@yield('content')
 			</div>
 			<!-- End: Content -->
--- app/views/page/sidebar.blade.php
+++ app/views/page/sidebar.blade.php
@@ -1,21 +1,77 @@
-<hr/>
-<p>
-MAIKO G. PASCUAL
-</p>
-<p>
-University of the Philippines Diliman
-B. Fine Arts, Visual Communication 2010
-Tarlac Local Designers Program 2011
-</p>
-<hr/>
-<p>
-Email: email@inkings.com
-</p>
-<hr/>
-<p>
-facebook: email@facebook.com
-</p>
-<hr/>
-<p>
-Address: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tristique
-</p>
+<style type="text/css">
+	.sidebar-content .logo {
+		margin-left: 35px;
+	}
+	.sidebar-content {
+		background-color: #B1B1B1;
+		padding: 10px;
+		position:fixed;
+		width: 215px;
+	}
+	/* Large desktop */
+		@media (min-width: 1200px) {
+		}
+
+		/* Portrait tablet to landscape and desktop */
+		@media (min-width: 768px) and (max-width: 979px) {
+			.sidebar-content {
+				position:relative;
+			}
+
+		}
+
+		/* Landscape phone to portrait tablet */
+		@media (max-width: 767px) {
+			.sidebar-content {
+				position:relative;
+				width: auto;
+			}
+		}
+
+		/* Landscape phones and down */
+		@media (max-width: 480px) {
+			.sidebar-content {
+				position:relative;
+			}
+		}
+</style>
+
+<div class="sidebar-content">
+	<!-- START: logo -->
+		<img src="images/logo.png" class="img-responsive logo"/>
+	<!-- END: logo -->
+	<hr/>
+	<h4>
+	MAIKO G. PASCUAL
+	</h4>
+	</p>
+	<p>
+	University of the Philippines Diliman
+	B. Fine Arts, Visual Communication 2010
+	Tarlac Local Designers Program 2011
+	</p>
+	<hr/>
+
+	<address>
+	  <strong>Address:</strong><br>
+	  795 Folsom Ave, Suite 600
+	  San Francisco, CA 94107
+	  <abbr title="Phone">P:</abbr> (123) 456-7890
+	</address>
+	<hr/>
+
+	<address>
+	  <strong>Email:</strong><br>
+	  <a href="mailto:#">first.last@email.com</a>
+	</address>
+	<hr/>
+
+	<address>
+	  <strong>Facebook:</strong><br>
+	  <a href="mailto:#">first.last@facebook.com</a>
+	</address>
+
+	<hr/>
+	Copyright &copy; 2014. All Rights Reserved.
+	<hr/>
+</div>
